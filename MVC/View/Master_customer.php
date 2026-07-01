fc88f85177f63101c1797cb8a8ce00e6fda5e83b
anhanh777 <trananhhunter02@gmail.com>
2026-07-01 22:21:02 +0700

    update master_customer

--- MVC/View/Master_customer.php
+++ MVC/View/Master_customer.php
@@ -24,7 +24,7 @@
     <div class="container-fluid px-lg-5">
         <a class="navbar-brand me-4" href="/web_qlsp/home">
 <div class="logo-coolmate-tech">
-  COOLMATE<span class="dot">.</span>
+  Duy__COOLMATE<span class="dot">.</span>
 </div>     </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
